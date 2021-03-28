43ebf856d93a34a834cefbc084ce8d738da45e49
Joonas Reinikka <joonas@fabricai.fi>
2021-03-28 22:12:04 +0300

    Add stuff

--- src/js/glitch/index.ts
+++ src/js/glitch/index.ts
@@ -2,17 +2,21 @@ export default class Glitch {
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
     playing: boolean;
+    y: number;
 
     constructor() {
         this.canvas = document.getElementById('glitch') as HTMLCanvasElement;
         this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
 
         this.playing = false;
+
+        this.y = 0;
     }
 
     private draw() {}
 
     private tick() {
+        this.draw();
         if (this.playing) {
             window.requestAnimationFrame(this.tick.bind(this));
         }
