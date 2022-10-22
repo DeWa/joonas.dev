8e3e7827d8e659e06b5de5120b208c42ecaffd3e
Joonas Reinikka <joonas.reinikka@gmail.com>
2022-10-22 14:25:21 +0300

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{njk,md}', './src/**/*.svg'],
+  theme: {
+    fontFamily: {
+      header: ['Montserrat', 'ui-sans-serif', 'sans'],
+      body: ['Quattrocento', 'system-ui', 'sans-serif'],
+      logo: ['Permanent Marker', 'cursive', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        darkestBlue: 'rgba(0, 19, 74, 1)',
+        darkestPink: 'rgba(35, 3, 41, 1) ',
+      },
+    },
+  },
+  plugins: [],
+};
