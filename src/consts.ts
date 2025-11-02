972377c9266c593aa5093ca566349414e39c8cce
Joonas Reinikka <joonas.reinikka@gmail.com>
2025-11-02 23:14:17 +0200

    Update consts These are still awful

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'JOONAS.DEV';
+export const SITE_DESCRIPTION = 'My personal website and blog';
