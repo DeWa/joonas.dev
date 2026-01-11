9e10b4df826cc4c183dc6b38d125414c08371a75
Joonas Reinikka <joonas.reinikka@gmail.com>
2026-01-12 00:06:15 +0200

    Add correct social links

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,7 @@
 export const SITE_TITLE = 'JOONAS.DEV';
 export const SITE_DESCRIPTION = 'My personal website and blog';
+export const MASTODON_LINK = 'https://mastodon.social/@dewanen';
+export const GITHUB_LINK = 'https://github.com/DeWa';
+export const BLUESKY_LINK = 'https://bsky.app/profile/dewanen.bsky.social';
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/joonas-reinikka-44358a44/';
+export const EMAIL_LINK = 'mailto:joonas.reinikka+joonasdev@gmail.com';
